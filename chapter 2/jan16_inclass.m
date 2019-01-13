0a6475aa938acc5ebcf3bd4f164408833770302b
John Horel <john.horel@utah.edu>
2019-01-13 15:18:44 -0700

    update

--- chapter 2/jan16_inclass.m
+++ chapter 2/jan16_inclass.m
@@ -1,8 +1,16 @@
-% jan16 2019 inclass assignment
 %clear all the variables
 clear all
 %close all the figure windows
 close all
+% jan16 2019 inclass assignment
+disp(' ')
+disp(' ')
+disp(' John Horel')
+disp(' ATMOS 5040/ Spring 2019')
+disp(' January 16 inclass assignment')
+disp(' Requires alta_snow.csv')
+
+
 %for info on the data
 %http://utahavalanchecenter.org/alta-monthly-snowfall
 %download the data from the class github repository
@@ -19,10 +27,10 @@ mmn = mean(data);
 % determine max value and index value of max for the winter season
 [maxs,mxi] = max(tot);
 % write to screen the year when max occurred
-fprintf('year of max seasonal snowfall and amount %f %f\n', yr(mxi),maxs)
+fprintf('year of max seasonal snowfall and amount %d %.1f cm\n', yr(mxi),maxs)
 %repeat for min 
 [mins,mni] = min(tot);
-fprintf('year of min seasonal snowfall and amount %f %f\n', yr(mni),mins)
+fprintf('year of min seasonal snowfall and amount %d %.1f cm\n', yr(mni),mins)
 
 %plot time series of snow totals as bar chart
 figure(1)
